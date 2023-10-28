5c9a9255ae64ef4f338629d25ab9eab9bbfb635d
Furkan Cingöz <efcingoz@outlook.com>
2023-10-28 12:11:01 +0300

    Tasarım ve celle bilgi aktarımı tamam

---
+++ FilmlerApp/Data/Entity/Filmler.swift
@@ -0,0 +1,8 @@
+//
+//  Filmler.swift
+//  FilmlerApp
+//
+//  Created by Furkan Cingöz on 28.10.2023.
+//
+
+import Foundation
---
+++ FilmlerApp/UI/Collection View/FilmlerHucre.swift
@@ -0,0 +1,12 @@
+//
+//  FilmlerHucre.swift
+//  FilmlerApp
+//
+//  Created by Furkan Cingöz on 28.10.2023.
+//
+
+import UIKit
+
+class FilmlerHucre: UICollectionViewCell {
+    
+}
