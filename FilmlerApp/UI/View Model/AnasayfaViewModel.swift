e60fda9e3e786ee096d8b7edae322882cffca8ee
Furkan Cingöz <efcingoz@outlook.com>
2023-11-12 14:01:47 +0300

    Mvvm mimarine taşıdık

--- FilmlerApp/UI/View Model/AnasayfaViewModel.swift
+++ FilmlerApp/UI/View Model/AnasayfaViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import RxSwift
+
+class AnasayfaViewModel {
+  var frepo = FilmlerDaoRepository()
+  var filmlerListesi = BehaviorSubject<[Filmler]>(value: [Filmler]())
+
+  init(){
+    filmlerListesi = frepo.filmlerListesi //burda ve repodaki filmler listesini birbirine bağladık
+    filmleriYukle()
+  }
+
+  func filmleriYukle() {
+    frepo.filmleriYukle()
+  }
+}
