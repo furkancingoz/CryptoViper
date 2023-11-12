843e8b33578c042ddc6fe73ba7ccedf46152a4cf
Furkan Cingöz <efcingoz@outlook.com>
2023-11-12 14:01:24 +0300

    Mvvm'e mimarisine geçtik

---
+++ FilmlerApp/Data/Repo/FilmlerDaoRepository.swift
@@ -0,0 +1,8 @@
+//
+//  FilmlerDaoRepository.swift
+//  FilmlerApp
+//
+//  Created by Furkan Cingöz on 12.11.2023.
+//
+
+import Foundation
---
+++ FilmlerApp/UI/View Model/AnasayfaViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnasayfaViewModel.swift
+//  FilmlerApp
+//
+//  Created by Furkan Cingöz on 12.11.2023.
+//
+
+import Foundation
